2e7090b3add7b37ba4f2fba4825e36c7211c88f5
Koen van Gils <koenvangils@gmail.com>
2021-02-03 07:47:27 +0000

    Changed frequencytable to list of lists.

--- scopelib.py
+++ scopelib.py
@@ -24,52 +24,52 @@ def correctdaterange(table,start,end):
         print('Start date can not be later than end date, no output.')
     return start,end
 
-def createfrequencytable(data,table1,table2,table3):
-    frequencytable=np.array([[1,2,3,4,5,6,7],[[],[],[],[],[],[],[]]])
-    for date in data:
+def createfrequencytable(daterange,table1,table2,table3):
+    frequencytable=[[],[],[],[],[],[],[]]
+    for date in daterange:
         weekday=date.dayofweek
         perdate=len(table1[table1.index.date==date.date()])+len(table2[table2.index.date==date.date()])+len(table3[table3.index.date==date.date()])
-        frequencytable[1,weekday].append(perdate)
+        frequencytable[weekday].append(perdate)
     return frequencytable
 
 def freqperweekday(table,scopetype,start,end):
     table=table[(table.index >= start) & (table.index < end)]
-    data=pd.date_range(start=start,end=end)
+    daterange=pd.date_range(start=start,end=end)
     table1=table[table['Endoscooptype'].isin(scopetype)]
     table2=table[table['Endoscooptype 2'].isin(scopetype)]
     table3=table[table['Endoscooptype 3'].isin(scopetype)]
     IDsoftypeinlog=table1['Endoscoop ID']
     IDsoftypeinlog=IDsoftypeinlog.append(table2['Endoscoop ID 2'])
     IDsoftypeinlog=IDsoftypeinlog.append(table3['Endoscoop ID 3'])  
-    frequencytable=createfrequencytable(data,table1,table2,table3)
+    frequencytable=createfrequencytable(daterange,table1,table2,table3)
     return frequencytable,IDsoftypeinlog.nunique()
 
 def freqperweekdayperuniquescope(table,serienummer,start,end):
-    table=table.loc[(table.index > start) & (table.index < end)]
-    data=pd.date_range(start=start,end=end)
+    table=table.loc[(table.index >= start) & (table.index < end)]
+    daterange=pd.date_range(start=start,end=end)
     table1=table[table['Serienr. endoscoop'].isin(serienummer)]
     table2=table[table['Serienr. endoscoop 2'].isin(serienummer)]
     table3=table[table['Serienr. endoscoop 3'].isin(serienummer)]
-    frequencytable=createfrequencytable(data,table1,table2,table3)
+    frequencytable=createfrequencytable(daterange,table1,table2,table3)
     return frequencytable
 
 def freqperweekdayperuser(table,scopetype,user,start,end):
-    table=table.loc[(table.index > start) & (table.index < end)]
-    data=pd.date_range(start=start,end=end)
+    table=table.loc[(table.index >= start) & (table.index < end)]
+    daterange=pd.date_range(start=start,end=end)
     table1=table[table['Endoscooptype'].isin(scopetype)]
     table1=table1[table1['Specialistnaam'].isin(user)]
     table2=table[table['Endoscooptype 2'].isin(scopetype)]
     table2=table2[table2['Specialistnaam 2'].isin(user)]
     table3=table[table['Endoscooptype 3'].isin(scopetype)]
     table3=table3[table3['Specialistnaam 3'].isin(user)]
-    frequencytable=createfrequencytable(data,table1,table2,table3)
+    frequencytable=createfrequencytable(daterange,table1,table2,table3)
     return frequencytable
 
 def boxplot(rawdata,scopetype,plotfile,start,end,qtyoftype=None):
     start,end=correctdaterange(rawdata[rawdata['Endoscooptype'].isin(scopetype)],start,end)
     frequencytable,qtyoftypeinlog=freqperweekday(rawdata,scopetype,start,end)
     fig, ax = plt.subplots()
-    ax.boxplot((np.array(frequencytable[1,0]), np.array(frequencytable[1,1]), np.array(frequencytable[1,2]), np.array(frequencytable[1,3]), np.array(frequencytable[1,4]), np.array(frequencytable[1,5]), np.array(frequencytable[1,6])),showmeans=True,whis=[0,100],zorder=2)
+    ax.boxplot((np.array(frequencytable[0]), np.array(frequencytable[1]), np.array(frequencytable[2]), np.array(frequencytable[3]), np.array(frequencytable[4]), np.array(frequencytable[5]), np.array(frequencytable[6])),showmeans=True,whis=[0,100],zorder=2)
     if qtyoftype:
         ax.set_title(plotfile.split('/')[-1]+'\nNr of scopes: '+str(qtyoftype)+' (Ultimo), '+str(qtyoftypeinlog)+' (Process Manager)\n Date range:'+str(start.date())+' - '+str(end.date()),fontsize=graphfontsize) 
     else:
@@ -87,9 +87,9 @@ def boxplot(rawdata,scopetype,plotfile,start,end,qtyoftype=None):
 def boxplotuniquescope(rawdata,serialnr,plotfile,start,end):
     start,end=correctdaterange(rawdata[rawdata['Serienr. endoscoop'].isin(serialnr)],start,end)
     frequencytable=freqperweekdayperuniquescope(rawdata,serialnr,start,end)
-    plt.boxplot((frequencytable[1,0],frequencytable[1,1],frequencytable[1,2],frequencytable[1,3],frequencytable[1,4],frequencytable[1,5],frequencytable[1,6]),showmeans=True,whis='range',zorder=2)
+    plt.boxplot((frequencytable[0],frequencytable[1],frequencytable[2],frequencytable[3],frequencytable[4],frequencytable[5],frequencytable[6]),showmeans=True,whis='range',zorder=2)
     plt.xticks( np.arange(1,8), ('Mo','Tu','We','Th','Fr','Sa','Su'))
-    plt.ylabel('Gebruiksfrequentie')
+    plt.ylabel('Use frequency')
     plt.title(plotfile.split('/')[-1]+'\n'+str(start.date())+' - '+str(end.date()),fontsize=graphfontsize)
     plt.savefig(plotfile+'.png')
     plt.close()
@@ -97,10 +97,10 @@ def boxplotuniquescope(rawdata,serialnr,plotfile,start,end):
 def boxplotperuser(rawdata,scopetype,user,plotfile,start,end):
     start,end=correctdaterange(rawdata[rawdata['Specialistnaam'].isin(user)],start,end)
     frequencytable=freqperweekdayperuser(rawdata,scopetype,user,start,end)
-    if np.max((frequencytable[1,0],frequencytable[1,1],frequencytable[1,2],frequencytable[1,3],frequencytable[1,4],frequencytable[1,5],frequencytable[1,6]))>0:
+    if np.max((frequencytable[0],frequencytable[1],frequencytable[2],frequencytable[3],frequencytable[4],frequencytable[5],frequencytable[6]))>0:
         plt.boxplot((frequencytable[1,0],frequencytable[1,1],frequencytable[1,2],frequencytable[1,3],frequencytable[1,4],frequencytable[1,5],frequencytable[1,6]),showmeans=True,whis='range',zorder=2)
         plt.xticks( np.arange(1,8), ('Mo','Tu','We','Th','Fr','Sa','Su'))
-        plt.ylabel('Gebruiksfrequentie')
+        plt.ylabel('Use frequency')
         plt.title(plotfile.split('/')[-1]+'\n'+str(start.date())+' - '+str(end.date()),fontsize=graphfontsize)
         plt.savefig(plotfile+'.png')
         plt.close()
